0cd9db8c1435dbbce7df492c8f20ce0535627248
ruchisharma12 <57267610+ruchisharma12@users.noreply.github.com>
2021-09-25 22:18:47 +0530

    Add files via upload

---
+++ W10_P3.py
@@ -0,0 +1,12 @@
+i=0
+x=[]
+try:
+	while(i<=5):
+		x.append(input("Please enter a number: "))
+		i=i+1
+	while (j<=10):
+		x[j]
+		j=j+1
+except IndexOutOfBound:
+	print("Index out of bound exception")
+	
